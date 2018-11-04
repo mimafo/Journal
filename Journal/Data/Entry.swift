ee16caf03d4b43e9213b05f632476b1c315d0d9f
Mike F <mikefolcher@gmail.com>
2018-11-04 10:28:00 -0500

    Model View Controller - Build Data struct Entry - Add data properties to Entry - Extend Entry by adding the equality interface - Build Data Controller EntryController - Add CRUD operations to the EntryContoller

---
+++ Journal/Data/Entry.swift
@@ -0,0 +1,43 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Michael Folcher on 11/4/18.
+//  Copyright © 2018 Mimafo. All rights reserved.
+//
+
+import UIKit
+
+struct Entry {
+    var title: String
+    var body: String
+    var tag: String
+    var color: UIColor
+    
+    mutating func update(title: String? = nil,
+                body: String? = nil,
+                tag: String? = nil,
+                color: UIColor? = nil) {
+        if let title = title {
+            self.title = title
+        }
+        if let body = body {
+            self.body = body
+        }
+        if let tag = tag {
+            self.tag = tag
+        }
+        if let color = color {
+            self.color = color
+        }
+    }
+}
+
+extension Entry: Equatable {
+    static func ==(left: Entry, right: Entry) -> Bool {
+        return left.title == right.body
+            && left.body == right.body
+            && left.tag == right.tag
+            && left.color == right.color
+    }
+}
---
+++ Journal/Data/EntryConroller.swift
@@ -0,0 +1,55 @@
+//
+//  EntryConroller.swift
+//  Journal
+//
+//  Created by Michael Folcher on 11/4/18.
+//  Copyright © 2018 Mimafo. All rights reserved.
+//
+
+import UIKit
+
+class EntryController {
+    
+    static let shared = EntryController()
+    let untagged = "Untagged"
+    
+    var entries: [Entry] = []
+    var tags: [String] {
+        return []
+    }
+    
+    // CRUD operations
+    func CreateEntry(withTitle title: String, body: String, tag: String? = nil, color: UIColor) {
+        let entry = Entry(title: title, body: body, tag: tag ?? untagged, color: color)
+        entries.append(entry)
+        //Save to disk
+    }
+    
+    func getEntry(at indexPath: IndexPath) -> Entry {
+        let tag = tags[indexPath.section]
+        let entries = getEntries(with: tag)
+        return entries[indexPath.row]
+    }
+    
+
+    
+    func updateEntry(_ entry: Entry, with title: String, body: String, tag: String? = nil, color: UIColor) {
+        guard let index = entries.firstIndex(of: entry) else { return }
+        entries[index].title = title
+        entries[index].body = body
+        entries[index].tag = tag ?? untagged
+        entries[index].color = color
+        //Save to disk
+    }
+    
+    func deleteEntry(_ entry: Entry) {
+        guard let index = entries.firstIndex(of: entry) else { return }
+        entries.remove(at: index)
+        //Save to disk
+    }
+    
+    // Internal helpers
+    private func getEntries(with tag: String) -> [Entry] {
+        return entries.filter { $0.tag == tag }
+    }
+}
