e4adaf3b9304a6252c4ca424b1c7aca5f54b0e2b
Mike F <mikefolcher@gmail.com>
2018-11-12 22:47:39 -0500

    Fix issue with UI Color View circles (somewhat) Add tap gesture to IU Color Views Add different colors to the UI Color Views

--- Journal/UI/ColorView.swift
+++ Journal/UI/ColorView.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
+protocol ColorViewDelegate: class {
+    func selectColor(customColor: UIColor)
+}
+
 class ColorView: UIView {
+    
+    var isSelected = false
+    var isSelectedView = UIView(frame: CGRect.zero)
+    var delegate: ColorViewDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,9 +29,59 @@ class ColorView: UIView {
     }
     
     func CreateView() {
+        //Create the view as a circle
         let radius = max(frame.width, frame.height) / 2
         layer.cornerRadius = radius
         clipsToBounds = true
-        backgroundColor = .red
+        backgroundColor = setupBackground()
+        
+        //Create the selected parts
+        isSelectedView.frame = CGRect(x: 10.0, y: 10.0, width: frame.width - 20.0, height: frame.height - 20.0)
+        let selectedRadius = max(isSelectedView.frame.width, isSelectedView.frame.height) / 2.0
+        isSelectedView.layer.cornerRadius = selectedRadius
+        isSelectedView.clipsToBounds = true
+        isSelectedView.backgroundColor = .black
+        isSelectedView.alpha = isSelected ? 0.5 : 0.0
+        
+        addSubview(isSelectedView)
+        setupTap()
     }
+    
+    func setupBackground() -> UIColor {
+        switch tag {
+        case 1:
+            return .red
+        case 2:
+            return .orange
+        case 3:
+            return .yellow
+        case 4:
+            return .green
+        case 5:
+            return .blue
+        case 6:
+            return .purple
+        case 7:
+            return .gray
+        default:
+            return .white
+        }
+    }
+    
+    func setupTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(selectColor))
+        addGestureRecognizer(tap)
+    }
+    
+    @objc func selectColor() {
+        delegate?.selectColor(customColor: setupBackground())
+        toggle()
+    }
+    
+    func toggle() {
+        isSelected = !isSelected
+        isSelectedView.alpha = isSelected ? 0.5 : 0.0
+        
+    }
+    
 }
